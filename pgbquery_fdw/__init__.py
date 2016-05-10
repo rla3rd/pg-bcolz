61b7e0c37c86b2819c2f33098ab99474cd12c189
Rick Albright <rla3rd@gmail.com>
2016-05-10 09:11:03 -0400

    ok the notebook version works for _where, so it must be something with the self.ctable in the wrapper

--- pgbquery_fdw/__init__.py
+++ pgbquery_fdw/__init__.py
@@ -33,7 +33,7 @@ class PgbqueryWrapper(ForeignDataWrapper):
     def __init__(self, options, columns):
         try:
             super(PgbqueryWrapper, self).__init__(options, columns)
-	    self.options = options
+            self.options = options
             self.columns = columns
             self.rootdir = self.options.get('rootdir', os.environ['PGBQUERY_HOME'])
             self.mode = self.options.get('mode', 'rw')
@@ -41,52 +41,30 @@ class PgbqueryWrapper(ForeignDataWrapper):
             self.row_id_column = self.options.get('primary_key', None)
             self.filter = ""
 
-            log_to_postgres(str(dict), INFO)
+            # log_to_postgres(str(self.options), INFO)
+            # log_to_postgres(str(self.columns), INFO)
       
             # somehow below i have to convert columns values ( pg column types )
             # into the appropiate dtypes to make an empty bcolz table
             # i'll open one up from disk for now
             self.ctable = pgbquery.open(self.rootdir, mode=self.mode)
         except:
-            errMsg = "%s: %s, rootdir: %s, mode: %s" % ( sys.exc_info()[0], traceback.format_exc(), self.rootdir, self.mode)
+            errMsg = "%s: %s, options: %s" % ( sys.exc_info()[0], traceback.format_exc(), self.options)
             log_to_postgres(errMsg, ERROR)
 
-    def _where(self, quals):
-        filters = []
-        for qual in quals:
+-
 
-            if not qual.is_list_operator:
-                if type(qual.value) == str:
-                    value = "'%s'" % qual.value
-                else:
-                    value = qual.value
-                filters.append((str(qual.field_name), str(qual.operator), str(value)))
-            else:
-                ANY = object()
-                ALL = object() 
-                op = qual.operator[0]
-                optype = qual.operator[1]
-                if op in ('=', '==') and optype == ANY:
-                    operator = 'IN'
-                if op in ('!=', '<>') and optype == ANY:
-                    operator = 'NOT IN'
-                    filters.append((qual.fieldname, operator, qual.value))
-
-            if len(quals) > 0:
-                boolarr = self.ctable.where_terms(filters)
-                return self.ctable.where(boolarr, outcols=test.names)
-            else:
-                return self.ctable.__iter__()
-
-    def execute(self, quals, columns):
-	rows = self._where(quals)
-	for row in rows:
-            yield row
-""" 
     def get_rel_size(self, quals, columns):
-	rows = self._where(quals)
-	estRows = length_hint(rows)
-	row = itertools.islice(rows, 1)
-	estWidth = estRows * sys.getsizeof(row)
-	return (estRows, estWidth)
-"""
+        log_to_postgres('get_rel_size quals: %s' % str(quals), INFO)
+        log_to_postgres('get_rel_size cols: %s' % str(columns), INFO)
+
+        rows = self._where(quals)     
+        estRows = length_hint(rows)
+        if rows != None:
+            row = itertools.islice(rows, 1)
+            estWidth = estRows * sys.getsizeof(row)
+        else:
+            estRows = 0
+            estWidth = 0
+
+        return (estRows, estWidth)
